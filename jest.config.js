12d1a8b08fc8d569fc6882b6f7ac4b1889bcdecf
Russ Atkins <russ@russatkins.co.uk>
2022-02-20 23:34:02 +0000

    change file structure

--- jest.config.js
+++ jest.config.js
@@ -10,5 +10,5 @@ module.exports = {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/graphql',
+  coverageDirectory: '../../coverage/apps/graphql',
 };
